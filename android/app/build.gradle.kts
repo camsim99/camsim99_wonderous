abf32fadb6f46d7e355d0eb8c659b36b11d96caa
Camille Simon <camillesimon@google.com>
2026-04-24 09:05:06 -0700

    update gradle stuff

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,9 +15,16 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        // javaParameters.set(true)
     }
+}
+
+    // kotlinOptions {
+    //     jvmTarget = JavaVersion.VERSION_17.toString()
+    // }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
